04ab0aa3cf2be63faf4672d7469c60c2b3574e03
Nathaniel Schweinberg <nathaniel@fightthecurrent.org>
2016-01-08 20:28:52 -0800

    initial commit

---
+++ podcast-clips.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Podcast Clips
+ * Version:           0.0.1
+ * Author:            Nathaniel Schweinberg
+ * Author URI:        http://fightthecurrent.org/
+ * License:           MIT
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
