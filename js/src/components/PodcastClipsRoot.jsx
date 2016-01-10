8be64995b6940cee541f2c105d1648f774fab92d
Nathaniel Schweinberg <nathaniel@fightthecurrent.org>
2016-01-09 21:06:03 -0800

    added dropzone component

--- js/src/components/PodcastClipsRoot.jsx
+++ js/src/components/PodcastClipsRoot.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Dropzone from 'react-dropzone';
 
 export default class PodcastClipsRoot extends React.Component {
+  onDrop(files) {
+    console.log('Received files: ', files);
+  }
   render() {
-    return <div>Loaded</div>;
+
+    return (
+        <div>
+        <Dropzone onDrop={this.onDrop}>
+        <div>Try dropping some files here, or click to select files to upload.</div>
+        </Dropzone>
+        </div>
+        );
   }
 }
