9fda5491f1d2c7a8083f8a9c70180b8588807266
Nathaniel Schweinberg <nathaniel@fightthecurrent.org>
2016-01-09 16:51:35 -0800

    updated webpack/npm configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,14 @@
 var path = require('path');
+var webpack = require('webpack');
+var merge = require('webpack-merge');
 
+const TARGET = process.env.npm_lifecycle_event;
 const PATHS = {
   app: path.join(__dirname, 'app'),
   build: path.join(__dirname, 'build')
 };
 
-module.exports = {
+const common = {
   entry: [
     'webpack/hot/dev-server',
     path.join(PATHS.app, 'main.js')
@@ -14,5 +17,25 @@ module.exports = {
     path: path.resolve(PATHS.build),
     publicPath: 'https://localhost:8080/', // this enables hot-update.json to be found.
     filename: 'bundle.js',
-  },
+  }
 };
+
+// Default configuration
+if(TARGET === 'dev' || !TARGET) {
+  module.exports = merge(common, {
+    devtool: 'eval-source-map',
+    devServer: {
+      historyApiFallback: true,
+      colors: true,
+      progress: true,
+      hot: true,
+      inline: true,
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only'
+    }
+  });
+}
+
+if(TARGET === 'build') {
+  module.exports = merge(common, {});
+}
