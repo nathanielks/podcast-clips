7e69fcd1215c8d3c231d710facb0dd5fb3df211f
Nathaniel Schweinberg <nathaniel@fightthecurrent.org>
2016-01-09 18:32:49 -0800

    added plugin description and renamed root file

--- wp-podcast-post-type.php
+++ wp-podcast-post-type.php
@@ -3,6 +3,7 @@
  *
  * @wordpress-plugin
  * Plugin Name:       WP Podcast Post Type
+ * Description:       A plugin that adds podcast and podcast_clip post types.
  * Version:           0.0.1
  * Author:            Nathaniel Schweinberg
  * Author URI:        http://fightthecurrent.org/
